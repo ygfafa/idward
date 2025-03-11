ff0977b6c139de9bacfad954b10268bc99682845
박종혁 <jonghyeok@jobis.co>
2025-03-11 11:33:15 +0900

    feat: change max width

--- components/mobile-screen.tsx
+++ components/mobile-screen.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { cn } from '../utils/cn'
 
-export const MAX_MobileScreen_WIDTH = 640
+export const MAX_MOBILE_SCREEN_WIDTH = 440
 
 type MobileScreenProps = {
   width?: number
@@ -14,7 +14,7 @@ export const MobileScreen = ({ className, style, children, ...props }: MobileScr
       className={cn('mx-auto flex min-h-full w-full flex-1 flex-col items-start', className)}
       style={{
         boxShadow: '-1px 0 0 0 #eee, 1px 0 0 0 #eee',
-        maxWidth: MAX_MobileScreen_WIDTH,
+        maxWidth: MAX_MOBILE_SCREEN_WIDTH,
         ...style,
       }}
       {...props}
