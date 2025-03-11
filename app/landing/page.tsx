d4c47e84a2060882b37990f320ac85b3a7b9622b
박종혁 <jonghyeok@jobis.co>
2025-03-11 12:31:01 +0900

    feat: install shadcn

---
+++ app/landing/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+
+const Page = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default Page
